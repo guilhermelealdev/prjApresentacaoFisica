1a0ac2a3a1035a3b4ea23cfcbed38421720fe629
Franca28 <felipe.oliveira.dev2025@gmail.com>
2026-04-12 21:33:10 -0300

    adição do footer da página

--- PrjFisica/src/components/Footer.jsx
+++ PrjFisica/src/components/Footer.jsx
@@ -14,5 +14,18 @@
  */
 
 export function Footer() {
-    
+    return (
+        <footer>
+            <h3>Feito pelo TIME DEV 2026</h3>
+
+            <h4>GitHub</h4>
+            <a href="https://github.com/Franca28">Felipe França</a>
+            <a href="https://github.com/jtahmartins-dev">João Henrique</a>
+            <a href="https://github.com/JMarquesDev">João Pedro</a>
+            <a href="https://github.com/guilhermelealdev">Guilherme Fagundes</a>
+
+            <h4>Projeto desenvolvido para o professor José Tiago Toledo do SESI CE-123</h4>
+            <a href="https://github.com/jtiagotoledo">GitHub</a>
+        </footer>
+    )
 }
