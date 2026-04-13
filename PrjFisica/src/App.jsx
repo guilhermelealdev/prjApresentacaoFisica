734119b6a4121d7b678c15410dc6b039e007ac7e
Guilherme F Dev <guilherme.leal.dev2025@gmail.com>
2026-04-13 18:28:58 -0300

    mudanças mínimas no app.css e app.jsx

--- PrjFisica/src/App.jsx
+++ PrjFisica/src/App.jsx
@@ -1,15 +1,19 @@
+import { NavBar } from "./components/NavBar"
 import { Header } from "./components/Header"
 import { Main } from "./components/Main"
 import { Footer } from "./components/Footer"
-import { NavBar } from './components/NavBar';
+import "./App.css"
 
 export default function App() {
 
   return (
     <div className="container">
     <NavBar />
+    <br />
     <Header />
+    <br />
     <Main />
+    <br />
     <Footer />
     </div>
   )
