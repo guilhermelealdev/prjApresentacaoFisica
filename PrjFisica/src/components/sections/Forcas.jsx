b29fa9ed1e8429d45bebc68644ade370005b1977
Guilherme F Dev <guilherme.leal.dev2025@gmail.com>
2026-04-14 17:00:48 -0300

    sections pronta

---
+++ PrjFisica/src/components/sections/Forcas.jsx
@@ -0,0 +1,336 @@
+export function Forcas() {
+  return (
+    <>
+      <h2>Forças - o que são?</h2>
+      <section className="explicacao">
+        <p>
+          No dia a dia, ouvimos a palavra "força" mas já se perguntou quantas
+          coisas a força pode ser? Nessa página, você terá a oportunidade de
+          aprender o conceito de força na física.
+        </p>
+      </section>
+
+      <img
+        src="https://img.freepik.com/vetores-premium/homem-forte-levantando-pesos-no-circo_911576-5780.jpg?semt=ais_hybrid&w=740&q=80"
+        alt="homem segurando um peso"
+      />
+
+      <br />
+
+      <section className="explicacao">
+        <p>
+          A força é uma <strong>grandeza vetorial</strong>, pois tem intensidade{" "}
+          {"(ou módulo)"}, direção e sentido. A unidade para se medir a
+          intensidade de uma força é o newton {"(N)"}
+        </p>
+      </section>
+      <br />
+
+      <img
+        src="src/assets/dinanometro.png"
+        alt="fotos de exemplo de um dinanometro"
+        class="imagemPequenas"
+      />
+      <br />
+
+      <section className="explicacao">
+        <p>
+          O aparelho usado para medir a intensidade das forças é o dinamômetro,
+          que funciona como uma mola que se deforma proporcionalmente a força
+        </p>
+      </section>
+      <br />
+
+      <h2>A direção e o sentido das forças</h2>
+
+      <section className="explicacao">
+        <p>
+          A força <strong>resultante</strong> é a força que pode substituir o
+          conjunto de forças aplicadas em um corpo, produzindo o mesmo efeito
+        </p>
+      </section>
+      <br />
+
+      <img
+        src="src/assets/resultante.png"
+        alt="duas pessoas empurrando um carro juntas"
+        className="imagemPequenas"
+      />
+      <br />
+
+      <section className="explicacao">
+        <p>
+          Quando duas ou mais forças são aplicadas ao mesmo ponto, na mesma
+          direção e mesmo sentido, a intensidade da força resultante é igual à
+          soma das forças, e a direção e o sentido são os mesmo das forças
+          aplicadas.
+        </p>
+      </section>
+      <br />
+
+      <section className="explicacao">
+        <p>
+          Quando duas forças são aplicadas em um ponto, com a mesma direção, mas
+          sentidos opostos, o valor da força resultante será dado pela diferença
+          do valor das forças.
+        </p>
+
+        <p>
+          O sentido da resultante será o mesmo que o da força de maior
+          intensidade.
+        </p>
+      </section>
+      <br />
+      <img
+        src="src/assets/mercado.png"
+        alt="crianças puxando carrinho de supermercado em direções opostas"
+        className="imagemPequenas"
+      />
+      <br />
+
+      <h2>Forças em direções diferentes</h2>
+      <br />
+
+      <section className="explicacao">
+        <p>
+          Quando duas ou mais forças são aplicadas em um ponto, em direções
+          diferentes, a força resultante é calculada da seguinte forma:
+        </p>
+
+        <p>
+          Representamos cada uma das forças por uma seta, no tamanho
+          proporcional ao módulo, e assim teremos uma situação como a
+          representada abaixo.
+        </p>
+
+        <p>
+          A resultante pode, então, ser calculada utilizando uma régua ou o
+          teorema de Pitágoras.
+        </p>
+      </section>
+      <br />
+
+      <img
+        src="src/assets/forcasdirecoesdiferentes.png"
+        alt="duas pessoas puxando um barco em direções diferentes"
+        className="imagemPequenas"
+      />
+      <br />
+
+      <section className="explicacao">
+        <p>
+          Com uma régua podemos medir as forças aplicadas e encontrar a força
+          resultante, de acordo com a escala usada.
+        </p>
+        <br />
+
+        <p>
+          Por exemplo: se tiver 5 cm, a resultante terá 50 newtons de
+          intensidade.
+        </p>
+        <br />
+
+        <p>
+          Se as duas forças formam um ângulo de 90°, a resultante será a
+          hipotenusa do triângulo retângulo formado.
+        </p>
+        <br />
+
+        <p>
+          Portanto, o modo mais eficiente de puxar o barco seria com as duas
+          garotas puxando juntas, pela mesma corda.
+        </p>
+      </section>
+      <br />
+      <img
+        src="src/assets/barco.png"
+        alt="duas pessoas puxando um barco na mesma direção"
+        className="imagemPequenas"
+      />
+      <br />
+      <img
+        src="src/assets/barcoformula.png"
+        alt="teorama de pitagoras do barco"
+        className="imagemPequenas"
+      />
+      <br />
+      <h2>A inércia de corpos</h2>
+      <br />
+      <section className="explicacao">
+        <p>
+          Todo corpo tem a tendência de permanecer em repouso ou em movimento
+          retilíneo uniforme se a resultante das forças que agem sobre ele for
+          nula.
+        </p>
+        <br />
+        <p>
+          Essa propriedade da matéria é chamada de inércia e está expressa na
+          primeira lei de Newton.
+        </p>
+      </section>
+
+      <img
+        src="src/assets/carrobatendo.png"
+        alt="carro batendo"
+        className="imagemPequenas"
+      />
+      <br />
+
+      <h2>Atrito</h2>
+
+      <section className="explicacao">
+        <p>
+          Na ausência de forças, um corpo pode continuar sempre em movimento
+          retilíneo uniforme.
+        </p>
+        <br />
+        <p>
+          Mas, se você der um peteleco em uma caneta sobre a mesa, ela logo para
+          de se movimentar.
+        </p>
+        <br />
+        <p>Por quê?</p>
+        <br />
+        <p>
+          A força de atrito da mesa sobre a caneta atua em sentido contrário ao
+          do movimento da caneta, opondo-se a ele.
+        </p>
+      </section>
+      <br />
+      <img
+        src="src/assets/atrito.png"
+        alt="bloco de metal sob uma mesa sendo puxado"
+        className="imagemPequenas"
+      />
+      <br />
+      <section className="explicacao">
+        <p>A força de atrito depende do tipo das superfícies em contato.</p>
+        <br />
+        <p>
+          Uma bolinha de metal, por exemplo, rola muito mais tempo sobre tampos
+          de vidro do que sobre tampos de cimento.
+        </p>
+        <br />
+        <p>Às vezes, é recomendável reduzir o atrito entre superfícies.</p>
+        <br />
+        <p>
+          Outras vezes queremos aumentar o atrito: por exemplo, entre os pneus
+          de um automóvel e o solo.
+        </p>
+        <br />
+        <p>
+          Nesse caso, quanto maior o atrito, menor a distância percorrida pelo
+          carro após a frenagem.
+        </p>
+      </section>
+      <br />
+      <img
+        src="src/assets/pneu.png"
+        alt="pneus empilhados"
+        className="imagemPequenas"
+      />
+      <br />
+      <section className="explicacao">
+        Aqui está o texto formatado seguindo o padrão estabelecido:
+        <p>Às vezes, é recomendável reduzir o atrito entre superfícies.</p>
+        <br />
+        <p>
+          Outras vezes queremos aumentar o atrito: por exemplo, entre os pneus
+          de um automóvel e o solo.
+        </p>
+        <br />
+        <p>
+          Nesse caso, quanto maior o atrito, menor a distância percorrida pelo
+          carro após a frenagem.
+        </p>
+      </section>
+      <br />
+      <h2>Força e aceleração</h2>
+      <br />
+      <section className="explicacao">
+        <p>
+          Forças provocam mudança na velocidade, ou seja, provocam acelerações.
+        </p>
+        <br />
+        <p>
+          A segunda lei de Newton diz que a aceleração que um corpo adquire é
+          diretamente proporcional à força resultante que atua sobre ele e tem a
+          mesma direção e o mesmo sentido dessa força.
+        </p>
+      </section>
+      <br />
+      <img
+        src="src/assets/puxandoferro.png"
+        alt="puxando um pedaço de metal sob a mesa acelerando"
+        className="imagemPequenas"
+      />
+      <br />
+      <section className="explicacao">
+        <p>
+          Quanto maior for a massa de um corpo, menor será a aceleração
+          provocada por determinada força.
+        </p>
+        <br />
+        <p>
+          A relação entre a massa e os módulos da força e da aceleração é dada
+          pela seguinte fórmula:
+        </p>
+      </section>
+      <br />
+
+      <img
+        src="src/assets/boliche.png"
+        alt="menina desizando vários objetos sob uma mesa"
+        className="imagemPequenas"
+      />
+      <br />
+      <br />
+      <h2>O peso e resistência do ar</h2>
+      <br />
+      <img
+        src="src/assets/pessoacaindo.png"
+        alt="pessoa caindo de paraquedas"
+        className="imagemPequenas"
+      />
+      <br />
+      <section className="explicacao">
+        <p>
+          Quando um corpo cai, a velocidade dele aumenta por causa da aceleração
+          da gravidade, mas deve-se considerar também a força de atrito com o
+          ar.
+        </p>
+        <br />
+        <p>
+          Após determinado tempo de queda, <strong>a resistência do ar</strong>{" "}
+          torna-se igual ao peso do corpo.
+        </p>
+        <br />
+        <p>
+          Como a resultante das forças que agem sobre esse corpo é nula, a
+          velocidade de queda não se altera mais.
+        </p>
+      </section>
+      <br />
+
+      <h2>O peso dos corpos</h2>
+      <br />
+      <section className="explicacao">
+        <p>
+          Os corpos caem com uma aceleração chamada de aceleração da gravidade,
+          que é provocada pela força da gravidade.
+        </p>
+        <br />
+        <p>
+          O peso é uma grandeza vetorial que pode ser calculada pela fórmula:
+        </p>
+      </section>
+      <br />
+
+      <img
+        src="src/assets/pedra.png"
+        alt="pedra caindo"
+        className="imagemPequenas"
+      />
+    </>
+  );
+}
